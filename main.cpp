b2496e240fba1e57d2182322a80ce8bdfa063587
Frysn <134208461+Frysn@users.noreply.github.com>
2025-02-17 10:36:46 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // string nama;
+    // string nama;
+    // string npm;
+    // string prodi;
+    // string fakultas;
+    // string univ;
+
+    // cout << "input data anda" << endl;
+    // cout << "Nama        : ";
+    // cin >> nama;
+    // cout << "NPM         : ";
+    // cin >> npm;
+    // cout << "Prodi       : ";
+    // cin >> prodi
+    // cout << "Fakultas    : ";
+    // cin >> fakultas;
+    // cout << "Universitas : ";
+    // cin >> univ;
+
+    // cout << endl;
+
+
+
+    // cout << "Nama        : " << nama << endl;
+    // cout << "NPM         : " << npm << endl;
+    // cout << "Prodi       : " << prodi <<endl;
+    // cout << "Fakultas    : " << fakultas << endl;
+    // cout << "Universitas : " << univ << endl;
+
+
+
+    int x = 10;
+    int y = 20;
+
+    int pertambahan = x+y;
+    int perkalian = x*y;
+    int pembagian = x/y;
+    int pengurangan = x-y;
+
+    cout << "Hasil Pertambahan dari " << x << " x " << y << " adalah " << pertambahan << endl;
+    cout << "Hasil Pengurangan dari " << x << " - " << y << " adalah " << pengurangan << endl;
+    cout << "Hasil Perkalian dari " << x << " x " << y << " adalah " << perkalian << endl;
+    cout << "Hasil Pembagian dari " << x << " / " << y << " adalah " << pembagian;
+    return 0;
+}
+
+
+
+
+
+
